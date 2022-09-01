3de42b96b2a3d5c0bdbbc771f2ee00b8e89525a0
fj <240968836@qq.com>
2022-09-01 16:23:05 +0800

    Todlist静态

---
+++ src/components/ShowOneThing.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" />
+      <span>xxxxx</span>
+    </label>
+    <button class="btn btn-danger" style="display: none">删除</button>
+  </li>
+</template>
+
+<script>
+    export default {
+    name: "ShowOneThing",
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/ShowTodos.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="todo-main">
+        <ShowOneThing/>
+    </ul>
+</template>
+
+<script>
+    import ShowOneThing from './ShowOneThing.vue'
+    
+    export default {
+        name:'ShowTodos',
+        components:{ShowOneThing}
+    }
+</script>
+
+<style>
+
+</style>
